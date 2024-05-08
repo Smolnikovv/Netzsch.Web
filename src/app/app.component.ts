a300c235b0316de3a4357bec884bbf312687134c
Marcin <thenickram@gmail.com>
2024-05-08 10:06:13 +0200

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignalrService } from './signalr.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,35 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'NetzschWeb';
+  paragraphText = "Output field";
+  signalrSubscription = new Subscription;
+
+  constructor(
+    public signalrService:SignalrService
+  )
+  {}
+  ngOnDestroy(): void {
+    this.signalrService.hubConnection?.off("");
+  }
+
+  ngOnInit(){
+    this.signalrService.startConnection();
+    this.signalrService.outputListiner();
+    this.signalrSubscription = this.signalrService.receivedText.subscribe((newText) => {
+      this.paragraphText = newText; 
+      console.log(this.paragraphText);
+    });
+  }
+
+  onInputChange(event: Event){
+    const inputElement = event.target as HTMLInputElement;
+    setTimeout(() => {
+      this.signalrService.inputSender(inputElement.value);
+    }, 500);
+   
+  }
+  
 }
+
---
+++ src/app/signalr.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NgZone } from '@angular/core';
+import * as signalR from '@microsoft/signalr'
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalrService {
+
+  constructor(private ngZone: NgZone) { 
+    this.startConnection();
+  }
+
+
+  hubConnection:signalR.HubConnection | undefined;
+  receivedText=new BehaviorSubject<string>('Output field');
+
+  startConnection = () => {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl('http://localhost:5016/signal', {
+      skipNegotiation: true,
+      transport: signalR.HttpTransportType.WebSockets
+    })
+    .build();
+
+    this.hubConnection
+    .start()
+    .then(() =>{
+      console.log('Connected to Hub');
+    })
+    .catch((err: string) => console.log('Failed with connection' + err))
+    this.outputListiner();
+  }
+
+  outputListiner = () => {
+    this.hubConnection?.on("SendWpfInput", (data: any) =>{
+      this.ngZone.run(() =>{
+        console.log("data recived: " + data);
+        this.receivedText.next(data);
+        console.log("variable: " + this.receivedText);
+      })     
+    })
+  }
+
+  inputSender(value:string){
+    this.hubConnection?.invoke("SendWebInput", value)
+      .catch(err => console.log(err));
+  }
+}
